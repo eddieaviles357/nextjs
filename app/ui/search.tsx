14972c5377cbeedc352f30432c3b898f71cc51f2
eduardo <eduardoaviles357@yahoo.com>
2024-01-10 21:01:33 -0800

    imported useDebouncedCallback from use-debounce module -> when ever the client presses a keystroke there will be a delay on everykeystroke to avoid making many request to our db ( this is called debouncing )

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,15 +2,17 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
-    const { replace } = useRouter();
 
+    console.log(term)
     if (term) {
       params.set('query', term);
     } else { // no string, delete
@@ -24,16 +26,19 @@ export default function Search({ placeholder }: { placeholder: string }) {
     thanks to Next.js's client-side navigation (which was learned about in the chapter on navigating between pages).
     */
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 350);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+      {/*ensures input field is in sync with the URL and will be populated when sharing*/}
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
