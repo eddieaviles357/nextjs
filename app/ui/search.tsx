ec4b8b2cc8e218d05a6823ea9fff41ad997e287a
eduardo <eduardoaviles357@yahoo.com>
2024-01-15 20:04:52 -0800

    search now updates page to 1

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,11 +6,12 @@ import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
+  const pathname = usePathname();
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     console.log(term)
     if (term) {
